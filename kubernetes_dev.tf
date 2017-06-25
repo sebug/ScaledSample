bc5e8a72798c5371f344bec651eb21d029f40e90
sebug <sebastian.gfeller@gmail.com>
2017-06-25 14:21:38 +0200

    Adding development VM

--- kubernetes_dev.tf
+++ kubernetes_dev.tf
@@ -10,6 +10,13 @@ resource "azurerm_public_ip" "kubernetesdev" {
   public_ip_address_allocation = "dynamic"
 }
 
+resource "azurerm_public_ip" "dockerdev" {
+  name = "remotingPublicIP2"
+  location = "West Europe"
+  resource_group_name = "${azurerm_resource_group.kubernetesdev.name}"
+  public_ip_address_allocation = "dynamic"
+}
+
 resource "azurerm_virtual_network" "kubernetesdev" {
   name = "acctvn"
   address_space = ["10.0.0.0/16"]
@@ -34,6 +41,19 @@ resource "azurerm_network_interface" "kubernetesdev" {
     subnet_id = "${azurerm_subnet.kubernetesdev.id}"
     private_ip_address_allocation = "dynamic"
     public_ip_address_id = "${azurerm_public_ip.kubernetesdev.id}"
+  }
+}
+
+resource "azurerm_network_interface" "dockerdev" {
+  name = "ddni"
+  location = "West Europe"
+  resource_group_name = "${azurerm_resource_group.kubernetesdev.name}"
+
+  ip_configuration {
+    name = "dockerdevconfiguration1"
+    subnet_id = "${azurerm_subnet.kubernetesdev.id}"
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id = "${azurerm_public_ip.dockerdev.id}"
   }
 }
 
@@ -76,6 +96,38 @@ resource "azurerm_virtual_machine" "kubernetesdev" {
   storage_os_disk {
     name = "myosdisk1"
     vhd_uri = "${azurerm_storage_account.kubernetesdev.primary_blob_endpoint}${azurerm_storage_container.kubernetesdev.name}/myosdisk1.vhd"
+    caching = "ReadWrite"
+    create_option = "FromImage"
+  }
+
+  os_profile {
+    computer_name = "containerdev"
+    admin_username = "sgfeller"
+    admin_password = "${var.containerdevpass}"
+  }
+
+  tags {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_virtual_machine" "dockerdev" {
+  name = "ddvm"
+  location = "West Europe"
+  resource_group_name = "${azurerm_resource_group.kubernetesdev.name}"
+  network_interface_ids = ["${azurerm_network_interface.dockerdev.id}"]
+  vm_size = "Standard_DS2_v2"
+
+  storage_image_reference {
+    publisher = "MicrosoftVisualStudio"
+    offer = "VisualStudio"
+    sku = "VS-2017-Comm-WS2016"
+    version = "latest"
+  }
+
+  storage_os_disk {
+    name = "myosdisk2"
+    vhd_uri = "${azurerm_storage_account.kubernetesdev.primary_blob_endpoint}${azurerm_storage_container.kubernetesdev.name}/myosdisk2.vhd"
     caching = "ReadWrite"
     create_option = "FromImage"
   }
