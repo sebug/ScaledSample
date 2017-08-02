b598eb87151a587651b6d7e404901d50c0aed3e7
sebug <sebastian.gfeller@gmail.com>
2017-08-02 21:32:06 +0200

    Can't ask for credentials immediately

--- create_kubernetes.sh
+++ create_kubernetes.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 az login
 az acs create --orchestrator-type=kubernetes --resource-group kubernetesdev --name=scaledSebugCluster --agent-count=2 --generate-ssh-keys --windows --admin-username azureuser --admin-password $1
-az acs kubernetes get-credentials --resource-group=kubernetesdev --name=scaledSebugCluster
+
